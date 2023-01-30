b4e850a6ba37301beba185d26cb670462f37e7df
Emiliano Alvarez <emilianoalvarez1987@gmail.com>
2023-01-30 03:57:46 -0300

    se agrega js

--- tareas.js
+++ tareas.js
@@ -1,1 +1,11 @@
 
+
+function myFunction() {
+    var x = document.getElementById("barranavegacion");
+    if (x.className === "barrasup") {
+      x.className += " responsiva";
+    } else {
+      x.className = "barrasup";
+    }
+  }
+
